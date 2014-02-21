556c34551703fc37360b6bc5a7d832992f20d6af
SherlockOy <oyang716@qq.com>
2014-02-21 21:27:53 +0800

    change packege

--- src/ArrangeRobot.java
+++ src/ArrangeRobot.java
@@ -6,22 +6,24 @@ public class ArrangeRobot {
 	 * 
 	 */
 	
-	ArrayList<ExamItem> examItems =  new ArrayList<ExamItem>();
-	ArrayList<Monitor> allMonitors = new ArrayList<Monitor>();
+	ArrayList<ExamItem> examItems =  new ArrayList();
+	ArrayList<Monitor> allMonitors = new ArrayList();
 
 
-	public void doArrangement() {
-		//
+	public void doArrangement(){
+		//调用IOperations的readXls将数据读入examItems和allMonitors
+        IOperations.readXls(ArrayList<ExamItem> examItem, ArrayList<Monitor> allMonitors);
+        // 
 	}
 
-	public int searchAvailableMonitor (ArrayList<Monitor> allMonitors,String department, int flag) {
+	public int searchAvailableMonitor(ArrayList<Monitor> allMonitors,String department, int flag) {
 		
 		//while-loop runs at most allMonitors.getlength() times
 		int i = allMonitors.size();
 		
-		while ( i>=0 ){
+		while(i>=0){
 			//i: control while-loop; flag indicates the beginning index in allMonitors
-			if ( (allMonitors.get(flag).timeLeft > 0) && allMonitors.get(flag).department.equals(department)) { 
+			if ((allMonitors.get(flag).timeLeft > 0) && allMonitors.get(flag).department.equals(department)) { 
 				//return the index of the nearest available monitor from the beginning flag
 				return flag;
 			}
